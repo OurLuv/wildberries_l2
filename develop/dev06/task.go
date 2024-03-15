d2a7ea8ba8feca60fddbbd3bd9a34ed76d889b81
ourluv <aorlowde@gmail.com>
2024-03-15 11:17:41 +0300

    finished 6 dev

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -13,6 +13,44 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	fields := flag.String("f", "", "Fields")
+	delimiter := flag.String("d", "\t", "Delimiter")
+	separated := flag.Bool("s", false, "Separated")
+	flag.Parse()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if *separated && !strings.Contains(line, *delimiter) {
+			continue
+		}
+
+		columns := strings.Split(line, *delimiter)
+
+		if *fields == "" {
+			fmt.Println(line)
+		} else {
+			var selectedFields []string
+			fieldNums := strings.Split(*fields, ",")
+			for _, v := range fieldNums {
+				index, _ := strconv.Atoi(v)
+				index -= 1
+				if index >= 0 && index < len(columns) {
+					selectedFields = append(selectedFields, columns[index])
+				}
+			}
+			fmt.Println(strings.Join(selectedFields, *delimiter))
+		}
+	}
 }
