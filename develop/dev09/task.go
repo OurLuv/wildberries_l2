e2bd8c779793db7d0b5bdeec630b2367cc37893a
ourluv <aorlowde@gmail.com>
2024-03-16 17:26:20 +0300

    finished 9 task

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
 /*
 === Утилита wget ===
 
@@ -8,6 +16,30 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func download(urlStr string) error {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create("index.html")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	file.Write(body)
+	return nil
+}
 
+func main() {
+	url := flag.String("url", "", "page to download")
+	flag.Parse()
+
+	if err := download(*url); err != nil {
+		fmt.Printf("error downloading page: %s", err)
+	}
 }
